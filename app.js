0511e859786d9821d7b7050a94658261c05be470
池晓生 <255979@qq.com>
2017-06-03 11:16:31 +0800

    image*3

--- app.js
+++ app.js
@@ -1,16 +1,25 @@
 //app.js
 App({
+  data: {
+    image: [
+      "http://img5.duitang.com/uploads/item/201510/18/20151018164145_GEfdr.jpeg",
+      "https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1496997300&di=35748f710660c11526e6dc96d76e38e7&imgtype=jpg&er=1&src=http%3A%2F%2Fimg3.duitang.com%2Fuploads%2Fitem%2F201505%2F11%2F20150511162020_8nXuR.jpeg",
+      "https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1496402581300&di=8b9eac0db28e461dc4f21e3912d88ae1&imgtype=0&src=http%3A%2F%2Fi0.hdslb.com%2Fbfs%2Farchive%2F651a92b0619929f589537d860112dd7ba24a6b27.jpg",
+
+    ]
+  },
+
   onLaunch: function () {
     //调用API从本地缓存中获取数据
     var logs = wx.getStorageSync('logs') || []
     logs.unshift(Date.now())
     wx.setStorageSync('logs', logs)
   },
-  getUserInfo:function(cb){
+  getUserInfo: function (cb) {
     var that = this
-    if(this.globalData.userInfo){
+    if (this.globalData.userInfo) {
       typeof cb == "function" && cb(this.globalData.userInfo)
-    }else{
+    } else {
       //调用登录接口
       wx.login({
         success: function () {
@@ -24,7 +33,8 @@ App({
       })
     }
   },
-  globalData:{
-    userInfo:null
+  globalData: {
+    userInfo: null
   }
 })
+var app = getApp()
